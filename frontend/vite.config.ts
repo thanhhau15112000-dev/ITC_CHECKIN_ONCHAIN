05bc60291c49efbb0337f4374e6f6d55f2c9f36a
Le Thanh Hau <thanhhau15112000@gmail.com>
2026-04-24 14:56:02 +0700

    update netlify

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -3,13 +3,15 @@ import react, { reactCompilerPreset } from '@vitejs/plugin-react'
 import babel from '@rolldown/plugin-babel'
 import path from "path"
 import tailwindcss from "@tailwindcss/vite"
+import netlify from '@netlify/vite-plugin'
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [
     react(),
     babel({ presets: [reactCompilerPreset()] }),
-    tailwindcss()
+    tailwindcss(),
+    netlify(),
   ],resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
